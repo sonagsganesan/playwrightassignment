b0be16f7c7242701206f310783c344d8214fb135
sonagsganesan <sonagsganesan1991@gmail.com>
2024-11-29 21:35:39 +0530

    add day1 assignment'

---
+++ HomeAssignment/Day01/DataTypes.js
@@ -0,0 +1,25 @@
+/* Assignment Details:
+Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`,
+including an uninitialized variable.
+Assignment Requirements:
+ Create the following variables using let (not using var) and check their typeOf
+ a) firstName
+ b) companyName
+ c) mobileNumber
+ d) isAutomation
+ e) hasPlaywright (do not assign)
+Print and confirm the values and data types */
+
+let firstName = "Sona";
+let companyName = "Acme Company";
+let mobileNumber = 898675652;
+let isAutomation = true;
+let hasPlaywright;
+let checkNullType = null;
+
+console.log(`Type of firstName is ${typeof(firstName)} and value is ${firstName}`)
+console.log(`Type of companyName is ${typeof(companyName)} and value is ${companyName}`)
+console.log(`Type of mobileNumber is ${typeof(mobileNumber)}and value is ${mobileNumber}`)
+console.log(`Type of isAutomation is ${typeof(isAutomation)} and value is ${isAutomation}`)
+console.log(`Type of hasPlaywright is ${typeof(hasPlaywright)} and value is ${hasPlaywright}`)
+console.log(`Type of checkNullType is ${typeof(checkNullType)} and value is ${checkNullType}`)
